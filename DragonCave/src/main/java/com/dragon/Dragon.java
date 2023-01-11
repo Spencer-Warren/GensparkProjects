ae357cd40f28ba239b5c1cae45ff159ee1679b23
Spencer A Warren <spencer.warren.19@cnu.edu>
2023-01-11 14:34:40 -0500

    Add Junit

--- DragonCave/src/main/java/com/dragon/Dragon.java
+++ DragonCave/src/main/java/com/dragon/Dragon.java
@@ -2,7 +2,7 @@ package com.dragon;
 
 import java.util.Scanner;
 
-public class Main {
+public class Dragon {
     private final static Scanner consoleIn = new Scanner(System.in);
     public static int getResponse() {
         int choice = 0;
@@ -15,33 +15,35 @@ public class Main {
         System.out.println("\n");
         return choice;
     }
+    private static String getCaveText(int choice) {
+        String path1 = "You approach the cave...\n" +
+                "It is dark and spooky...\n" +
+                "A large dragon jumps out in front of you! He opens his jaws and...\n" +
+                "Gobbles you down in one bite!\n";
+
+        String path2 = "You approach the cave...\n" +
+                "It is dark and spooky...\n" +
+                "A large dragon jumps out in front of you! He opens his jaws and...\n" +
+                "Offers you a handful of gold!\n";
+
+        if (choice == 1) {
+            return path1;
+        }
+        else {
+            return path2;
+        }
+    }
     public static void main(String[] args) {
         String intro = "You are in a land full of dragons. In front of you,\n" +
                         "you see two caves. In one cave, the dragon is friendly\n" +
                         "and will share his treasure with you. The other dragon\n" +
                         "is greedy and hungry and will eat you on sight.\n" +
                         "Which cave will you go into? (1 or 2)\n";
-
-        String path1 = "You approach the cave...\n" +
-                        "It is dark and spooky...\n" +
-                        "A large dragon jumps out in front of you! He opens his jaws and...\n" +
-                        "Gobbles you down in one bite!\n";
-
-        String path2 = "You approach the cave...\n" +
-                        "It is dark and spooky...\n" +
-                        "A large dragon jumps out in front of you! He opens his jaws and...\n" +
-                        "Offers you a handful of gold!\n";
-
         // Intro string
         System.out.println(intro);
 
         int choice = getResponse();
-
-        if (choice == 1) {
-            System.out.println(path1);
-        }
-        else {
-            System.out.println(path2);
-        }
+        String text = getCaveText(choice);
+        System.out.println(text);
     }
 }
